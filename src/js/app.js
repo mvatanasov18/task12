fecd04fb48d01d5a4d59d614a58c7ceccd1cd0db
mvatanasov18 <54852701+mvatanasov18@users.noreply.github.com>
2022-11-29 21:30:54 +0200

    Change title after 3 seconds

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,9 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  setTimeout(()=>{
+    document.title = "One new message";
+  },3000)
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
